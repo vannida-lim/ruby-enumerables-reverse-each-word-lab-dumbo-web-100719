60dbf9ef2818f265a4198e1f027826ece6ffe82d
Vannida Lim <vannida731@gmail.com>
2019-09-14 06:27:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,12 @@
 def reverse_each_word(sentence)
   words = sentence.split(/ /)
   
-  reversed = []
+  reversed_words = []
   words.each do {|index|}
-    currentword = words[index]
-    reversed_word = currentword.!reverse
+    current_word = words[index]
+    reversed_word = current_word.reverse!
+    reversed_words.push(reversed_word)
   end
 
- 
+  return reversed_words.join(/ /)
 end
