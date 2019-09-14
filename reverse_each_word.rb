960efd639f40fa1df9eaf8627d654cbb04213905
Vannida Lim <vannida731@gmail.com>
2019-09-14 07:00:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,19 +4,5 @@ def reverse_each_word(sentence)
   last_word = words[words.length-1]
 
   words.each do |current_word|
-    reversed_word = current_word.reverse!
-    
-    reversed_sentence += reversed_word
-    
-    #only add space if were not at last word
-    if current_word == last_word
-      puts 'reached last word!'
-      break
-    end
-    reversed_sentence += ' '
-  end
- 
-  
-  puts reversed_sentence
-  return reversed_sentence
+    r
 end
