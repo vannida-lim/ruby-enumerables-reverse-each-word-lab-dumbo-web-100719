a698f8010aa6f7efae3e579da39ef6e19a54809f
Vannida Lim <vannida731@gmail.com>
2019-09-14 06:32:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,7 +9,7 @@ def reverse_each_word(sentence)
     reversed_words.push(reversed_word)
   end
 
-  puts 'result list ---'
+  puts 'result list >>>>>'
   puts reversed_words
   return reversed_words.join(/ /)
 end
