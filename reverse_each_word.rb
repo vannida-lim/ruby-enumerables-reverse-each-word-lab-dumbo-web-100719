ea4c54b839b0f279e5c537005c2dfb7cce529041
Vannida Lim <vannida731@gmail.com>
2019-09-14 06:15:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   sentence = sentence.split(/  /)
   reversed = []
-  sentence.each do |sentence|
+  sentence.each do {|sentence|}
     puts sentence.reverse
   end
 end
