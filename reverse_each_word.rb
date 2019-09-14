94e207db6fb54be9f29e51cb030511b5d118138a
Vannida Lim <vannida731@gmail.com>
2019-09-14 06:21:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   reverse = sentence.split(/  /).reverse()
-  puts reverse 
+  puts 'hihihi' 
   # reversed = []
   # sentence.each do {|sentence|}
   #   puts sentence.reverse
